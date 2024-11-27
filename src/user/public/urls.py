db1ff0b4fd9055a7f99491d9da3e1756325e244e
Bibek Joshi <bibekjoshi34@gmail.com>
2024-11-27 22:03:53 +0545

    user signup

--- src/user/public/urls.py
+++ src/user/public/urls.py
@@ -2,16 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    PublicUserChangePasswordView,
-    PublicUserForgetPasswordRequestView,
-    PublicUserForgetPasswordView,
-    PublicUserLoginView,
-    PublicUserLogoutView,
-    PublicUserProfileUpdateView,
-    PublicUserProfileView,
     PublicUserSignUpAPIView,
-    PublicUserTokenRefreshView,
-    PublicUserVerifyAccountAPIView,
 )
 
 router = DefaultRouter(trailing_slash=False)
@@ -20,40 +11,7 @@ urlpatterns = [
     path(
         "users/signup",
         PublicUserSignUpAPIView.as_view(),
-        name="public_user_login",
-    ),
-    path("users/login", PublicUserLoginView.as_view(), name="public_user_login"),
-    path("users/logout", PublicUserLogoutView.as_view(), name="public_user_logout"),
-    path("users/profile", PublicUserProfileView.as_view(), name="public_user_profile"),
-    path(
-        "users/account/verify",
-        PublicUserVerifyAccountAPIView.as_view(),
-        name="public_verify_account",
-    ),
-    path(
-        "users/profile/update",
-        PublicUserProfileUpdateView.as_view(),
-        name="public_user_profile_update",
-    ),
-    path(
-        "users/token/refresh",
-        PublicUserTokenRefreshView.as_view(),
-        name="public_token_refresh",
-    ),
-    path(
-        "users/change-password",
-        PublicUserChangePasswordView.as_view(),
-        name="public_user_change_password",
-    ),
-    path(
-        "users/forget-password-request",
-        PublicUserForgetPasswordRequestView.as_view(),
-        name="public_user_forget_password_request",
-    ),
-    path(
-        "users/forget-password",
-        PublicUserForgetPasswordView.as_view(),
-        name="public_user_forget_password",
+        name="public_user_signup",
     ),
     path("", include(router.urls)),
 ]
