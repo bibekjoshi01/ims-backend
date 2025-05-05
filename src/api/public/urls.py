9e31bb78e2f45edd502a3ae31f8b8cd5562f5189
Bibek Joshi <bibekjoshi34@gmail.com>
2025-05-05 14:24:39 +0545

    public api removed

--- src/api/public/urls.py
+++ src/api/public/urls.py
@@ -2,7 +2,4 @@ from django.urls import include, path
 
 app_label = ["public"]
 
-urlpatterns = [
-    # path("blog-app/", include("src.blog.public.urls")),
-    path("user-app/", include("src.user.public.urls")),
-]
+urlpatterns = []
