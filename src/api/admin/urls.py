5ba47159884b7acbc1990cda12e4f260f66ca8c8
bibekjoshi01 <bibekjoshi34@gmail.com>
2025-06-02 11:38:14 +0545

    minor refactoring

--- src/api/admin/urls.py
+++ src/api/admin/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("user-app/", include("src.user.urls")),
     path("blog-app/", include("src.blog.urls")),
     path("supplier-mod/", include("src.supplier.urls")),
+    path("customer-mod/", include("src.customer.urls")),
 ]
